5cfc32719c0f2f45cc6f4aefcc22ce6f5c2aaa04
Drup <drupyog@zoho.com>
2015-04-09 00:56:47 +0200

    A bit more documentation.

--- src/llvm2smt.mli
+++ src/llvm2smt.mli
@@ -6,14 +6,28 @@
       module Smtg = Smtgraph.Make (ZZ3)
       module L2S = Llvm2smt.Init (ZZ3) (Smtg)
     ]}
+
+    Then, assuming you have [llf] an llvm function:
+    {[
+      let smt =
+        let llb2node, llg = Llvmcfg.of_llfunction llf in
+        let breakpoints, llg' = Llvmcfg.break_graph llg in
+        let smtg = L2S.llvm2smt llf points llg' in
+        SMTg.to_smt smtg
+    ]}
+
+    Note that this functor *is* stateful. It will build up the environment [env] as it translate functions which allows to find the smt variable from an llvm variable.
 *)
 
+(** Raised if an llvm instruction is not implemented. *)
 exception Not_implemented of Llvm.llvalue
 val sprint_exn : Llvm.llvalue -> string
 
+(** Raised if an llvm variable is used in the control flow graph but doesn't exist. *)
 exception Variable_not_found of (bool * Llvm.llvalue)
 val sprint_exn_var : bool * Llvm.llvalue -> string
 
+(** Raised if an llvm basic bloc is used in the control flow graph but doesn't exist. *)
 exception Block_not_found of (bool * Llvm.llbasicblock)
 val sprint_exn_block : bool * Llvm.llbasicblock -> string
 
